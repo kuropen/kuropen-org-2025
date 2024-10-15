cb5f9ffeeb812e6a76332994492ed612de4b5226
Hirochika Yuda <hyuda@outlook.jp>
2024-10-15 16:28:28 +0900

    Batch token register

---
+++ database/migrations/2024_10_15_071045_create_misskey_batch_tokens_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('misskey_batch_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->string('token')->unique();
+            $table->boolean('is_admin')->default(false);
+            $table->string('permission');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('misskey_batch_tokens');
+    }
+};
