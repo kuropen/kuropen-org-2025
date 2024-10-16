6f1fa281bf6017f824b63bde7e0bfc0bed94d5bb
Hirochika Yuda <hyuda@outlook.jp>
2024-10-16 19:37:28 +0900

    Fix IP address fetch method behind Cloudflare

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,11 @@
 <?php
 if (!function_exists('compare_url')) {
+    /**
+     * URLのスキームとホストが一致しているかどうかを比較する.
+     * @param $url1
+     * @param $url2
+     * @return bool
+     */
     function compare_url($url1, $url2): bool
     {
         $url1 = parse_url($url1);
@@ -14,5 +20,21 @@ if (!function_exists('compare_url')) {
         }
 
         return true;
+    }
+}
+
+if (!function_exists('get_client_ip')) {
+    /**
+     * クライアントのIPアドレスを取得する.
+     * この関数はCloudflareのCF-Connecting-IPヘッダーを考慮しています.
+     * @return string
+     */
+    function get_client_ip(): string
+    {
+        $request = request();
+        if ($request->hasHeader('CF-Connecting-IP')) {
+            return $request->header('CF-Connecting-IP');
+        }
+        return $request->ip();
     }
 }
