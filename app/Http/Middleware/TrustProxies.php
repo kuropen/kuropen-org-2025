eaa21327987294a1a237cf3f7f78999b7e387d5f
Hirochika Yuda <hyuda@outlook.jp>
2024-10-19 08:06:39 +0900

    Change TrustedProxy Setting

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -13,17 +13,20 @@ class TrustProxies extends Middleware
      * @var array<int, string>|string|null
      */
     protected $proxies;
-    // WARNING: index.php で CF-Connecting-IP を REMOTE_ADDR に代入するため、TrustedProxiesは設定しないこと
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    protected $headers =
-        Request::HEADER_X_FORWARDED_FOR |
-        Request::HEADER_X_FORWARDED_HOST |
-        Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO |
-        Request::HEADER_X_FORWARDED_AWS_ELB;
+    protected $headers = Request::HEADER_X_FORWARDED_PROTO;
+//        Request::HEADER_X_FORWARDED_FOR |
+//        Request::HEADER_X_FORWARDED_HOST |
+//        Request::HEADER_X_FORWARDED_PORT |
+//        Request::HEADER_X_FORWARDED_PROTO |
+//        Request::HEADER_X_FORWARDED_AWS_ELB;
+
+    // WARNING: index.php で CF-Connecting-IP を REMOTE_ADDR に代入するため、
+    // X-Forwarded-For, X-Forwarded-Host は使わない。
+    // X-Forwarded-Proto は、 https を検出するために使う。
 }
