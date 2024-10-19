fd271d8991b12605005a13712b7f01e426af1708
Hirochika Yuda <hyuda@outlook.jp>
2024-10-19 09:42:08 +0900

    Set RemoteIPInternalProxy

--- docker-resource/gen_remote_ip.sh
+++ docker-resource/gen_remote_ip.sh
@@ -3,3 +3,4 @@
 echo 'RemoteIPHeader CF-Connecting-IP'
 echo 'RemoteIPTrustedProxy' $(curl -s https://www.cloudflare.com/ips-v4 | tr '\n' ' ')
 echo 'RemoteIPTrustedProxy' $(curl -s https://www.cloudflare.com/ips-v6 | tr '\n' ' ')
+echo 'RemoteIPInternalProxy 100.64.0.0/10'
