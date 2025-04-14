a8adc8b18ec6371fa4098991b7d2633b841f1ad3
Hirochika Yuda <hyuda@outlook.jp>
2025-04-15 08:15:19 +0900

    Categorize data sources for future API implementation

--- config/const.php
+++ config/const.php
@@ -23,6 +23,16 @@ return [
         'api_url_prefix' => 'https://sizu.me/api/v1',
         'api_key' => env('SIZU_ME_API_KEY'),
     ],
+    'dataSources' => [
+        'literatures' => [
+            'sizu.me',
+            'Classic Notes Archive',
+        ],
+        'microblogs' => [
+            'bluesky',
+            'misskey',
+        ],
+    ],
     'misskey' => [
         'availability_key' => 'misskey_available',
         'notification_account' => env('INQUIRY_NOTIFICATION_ACCOUNT', 'info'),
